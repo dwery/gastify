e42f7d08b53680ecf757790ec6fd4eaee2d1a4d3
penschuck <penschuck@8a573288-591e-0410-a4c7-0d60402f24be>
2006-10-07 15:18:36 +0000

    initial import

---
+++ trunk/src/gui.h
@@ -0,0 +1,8 @@
+/* This file was automatically generated.  Do not edit! */
+void onClearHistory();
+void onShowAbout(GtkWidget *widget,gpointer user_data);
+void addToHistory(gchar *call);
+void notifyPopup(char *notifyMessage,GtkStatusIcon *icon);
+void onShowHistory();
+void activateMenu();
+GtkStatusIcon *initializeGui();
