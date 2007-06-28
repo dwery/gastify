f011b125ee2cbae9a2c606dcbe062abf7007e25f
penschuck <penschuck@8a573288-591e-0410-a4c7-0d60402f24be>
2007-06-28 18:53:01 +0000

    some gettext changes

--- trunk/src/socket.c
+++ trunk/src/socket.c
@@ -1,23 +1,23 @@
-/** 
+/**
  *
- * Copyright (C) 2006-2007 by Jan Penschuck                     
+ * Copyright (C) 2006-2007 by Jan Penschuck
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Library General Public License
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
- 
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -42,18 +42,18 @@ gboolean watcher(GIOChannel *source, GIOCondition condition, gpointer user_data)
 	gsize read;
 	gchar *cidnum, *cidname, *called;
 	g_io_channel_read_line(source, &buffer, &read, NULL, NULL);
-	
+
 	if (strchr(buffer,'|')) {
 			cidnum = cidname = called = NULL;
-		
+
 			cidnum = strsep(&buffer, "|");
 			cidname = strsep(&buffer, "|");
 			called = strsep(&buffer, "\0");
 
-			notifyMessage = g_strconcat( _("call from:\n"), cidnum, ", ", cidname, "\n", NULL);			
+			notifyMessage = g_strconcat( _("call from: "), cidnum, ", ", cidname, "\n", NULL);
 			notifyPopup(notifyMessage, user_data);
 			addToHistory(notifyMessage);
-	} 
+	}
 	else {
 			fprintf(stderr, _("this is not a gastify message. waiting...\n"));
 	}
@@ -72,16 +72,16 @@ void watchSocket(int port, GtkStatusIcon *icon) {
 		printf(_("cannot open socket\n"));
 		exit(1);
 	}
-  
+
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_addr.s_addr = INADDR_ANY;
-  servAddr.sin_port = htons(port);
+	servAddr.sin_port = htons(port);
 
  	if ((rc = bind (sd, (struct sockaddr *) &servAddr,sizeof(servAddr)))<0 || servAddr.sin_port==0) {
 	  printf(_("cannot bind port %d\n"), ntohs(servAddr.sin_port));
 	  exit(1);
 	}
-	
+
 	GIOChannel *gchannel = g_io_channel_unix_new(sd);
 	g_io_add_watch(gchannel, G_IO_IN, watcher, icon);
 
