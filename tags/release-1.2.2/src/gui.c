5fb508c6dfaabf32783862f2dbce573e56972b36
penschuck <penschuck@8a573288-591e-0410-a4c7-0d60402f24be>
2008-09-14 09:45:33 +0000

    tagging last release

---
+++ tags/release-1.2.2/src/gui.c
@@ -0,0 +1,166 @@
+/**
+ *
+ * Copyright (C) 2006-2007 by Jan Penschuck
+ *
+ * Gastify is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * Gastify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <string.h>
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+#include <glade/glade-xml.h>
+#include <libnotify/notify.h>
+#include <libnotify/notification.h>
+#include <gtk/gtkstatusicon.h>
+#include "main.h"
+#include "socket.h"
+#include "gui.h"
+
+#define VERSION "1.2.2"
+#define APPNAME "gastify"
+
+static GtkWidget *menu;
+static GtkWidget *historyDialog;
+static GtkWidget *textView;
+static GtkTextBuffer *buffer;
+
+GtkStatusIcon* initializeGui() {
+
+	/* initialize statusicon */
+	GtkStatusIcon *icon;
+	GladeXML *xml;
+	int x, y;
+	gboolean push_in;
+	
+	icon = gtk_status_icon_new_from_icon_name("gastify");
+	gtk_status_icon_set_tooltip(icon, _("gastify call notification"));
+
+	/* load menu */
+	xml = glade_xml_new("/usr/share/gastify/gastify.glade", "menu1", "gastify");
+	glade_xml_signal_autoconnect(xml);
+	menu = glade_xml_get_widget(xml, "menu1");
+
+	xml = glade_xml_new("/usr/share/gastify/gastify.glade", "window1", "gastify");
+	glade_xml_signal_autoconnect(xml);
+	historyDialog = glade_xml_get_widget(xml, "window1");
+	g_signal_connect(historyDialog, "delete-event", GTK_SIGNAL_FUNC(gtk_widget_hide_on_delete), NULL);
+	textView = glade_xml_get_widget(xml, "textview1");
+	buffer = gtk_text_buffer_new(NULL);
+
+	gtk_status_icon_position_menu((GtkMenu*)menu, &x, &y, &push_in, icon);
+	g_signal_connect(icon, "popup-menu", GTK_SIGNAL_FUNC(activateMenu), NULL);
+	g_signal_connect(icon, "activate", GTK_SIGNAL_FUNC(onShowHistory), icon);
+
+	return icon;
+}
+
+/* show libnotify popup */
+void notifyPopup(gchar *notifyMessage, GtkStatusIcon *icon) {
+
+	NotifyNotification *notify;
+
+	notify_init("gastify");
+	notify = notify_notification_new("gastify", notifyMessage, NULL, NULL);
+	notify_notification_set_timeout(notify, popuptime*1000);
+	notify_notification_attach_to_status_icon(notify, icon);
+	notify_notification_show(notify, NULL);
+	g_object_unref(G_OBJECT(notify));
+	gtk_status_icon_set_from_icon_name(icon, "gastify_new_call");
+
+}
+
+/* log call to buffer */
+void addToHistory(gchar *call) {
+
+	GtkTextIter iter;
+	gchar timestamp[255];
+	time_t timet;
+	gchar *line;
+
+	/* get timestamp and assemble line*/
+	time(&timet);
+	strftime(timestamp, 64, "%a %H:%M \t", localtime(&timet));
+	line = g_strconcat(timestamp, " ", call, NULL);
+
+	/* write to GtkTextView */
+	gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
+	gtk_text_buffer_insert(buffer, &iter, line, -1);
+	gtk_text_view_set_buffer((GtkTextView*)textView, buffer);
+	
+	g_free(line);
+
+}
+
+/* open menu */
+void activateMenu() {
+
+	gtk_menu_popup((GtkMenu*)menu, NULL, NULL, NULL, NULL, 0, gtk_get_current_event_time());
+
+}
+
+/* show about */
+void onShowAbout() {
+
+	gchar *license = g_strconcat("\nGastify is free software; you can redistribute it and/or modify it under", 
+															"the terms of the GNU General Public License as published by the Free Software Foundation",
+															"either version 2 of the License, or (at your option) any later version.\n\r",
+							 								"Gastify is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; ",
+							 								"without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. ",
+							 								"See the GNU General Public License for more details.\n\r You should have received a copy ",
+							 								"of the GNU General Public License along with Gastify; if not, write to the ",
+							 								"Free Software Foundation, Inc.,59 Temple Place, Suite 330, Boston, MA  02111-1307  USA",
+															NULL);
+	
+	static const char *authors[] = {"Jan Penschuck","penschuck@gmail.com", NULL};
+	gtk_show_about_dialog(NULL,
+							"name", APPNAME,
+							"version", VERSION,
+							"license", license,
+							"wrap-license", TRUE,
+							"copyright", "\xc2\xa9 2006-2007 Jan Penschuck",
+							"comments", _("a client for app-notify"),
+							"logo-icon-name", "gastify",
+							"website", "http://code.google.com/p/gastify/",
+							"website-label", "gastify website",
+							"authors", authors,
+							NULL);
+					
+	g_free(license);
+
+}
+
+/* show history */
+void onShowHistory(gpointer *data) {
+
+	gtk_status_icon_set_from_icon_name((GtkStatusIcon*)data, "gastify");
+	if ( !GTK_WIDGET_VISIBLE(historyDialog) ) {
+		gtk_widget_show(historyDialog);
+	} else {
+		gtk_widget_hide(historyDialog);
+	}
+
+}
+
+/* clear buffer */
+void onClearHistory() {
+
+	buffer = gtk_text_buffer_new(NULL);
+	gtk_text_view_set_buffer((GtkTextView*)textView, buffer);
+
+}
+
---
+++ tags/release-1.2.2/src/main.c
@@ -0,0 +1,105 @@
+/* Mi Jul 26 17:50:21 UTC 2006 */
+
+/***************************************************************************
+ * Copyright (C) 2006-2007 by Jan Penschuck                                                    
+ *
+ *
+ * Gastify is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * Gastify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <locale.h>
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include "socket.h"
+#include "gui.h"
+
+#define VERSION "1.2.3"
+#define APPNAME "gastify"
+	
+static gint port = 40000;
+static gboolean daemonize = FALSE;
+static gboolean version = FALSE;
+gint   popuptime = 10;
+
+static GOptionEntry entries[] = 
+{
+	{ "port", 'p', 0, G_OPTION_ARG_INT, &port, "listen on given port", "N" },
+	{ "time", 't', 0, G_OPTION_ARG_INT, &popuptime, "display popup given time", "SECONDS" },
+	{ "demonize", 'd', 0, G_OPTION_ARG_NONE, &daemonize, "forks in the background", NULL },
+	{ "version", 'V', 0, G_OPTION_ARG_NONE, &version, "print version info", NULL },
+	
+};
+
+int main(int argc, char *argv[]) {
+	
+	/* commandline options */
+	GOptionContext *context;
+
+	context = g_option_context_new ("");
+ 	g_option_context_add_main_entries(context, entries, "gastify");
+	g_option_context_add_group (context, gtk_get_option_group (TRUE));
+	g_option_context_parse (context, &argc, &argv, NULL);
+	
+	/* print version info */		
+	if (version) {
+		printf("%s %s\n", APPNAME, VERSION);
+		exit(0);		
+	}
+
+	/* demonize or not */
+	if (daemonize) {				
+		pid_t   pid, sid;
+		pid = fork();
+	
+		if (pid < 0) {
+			exit(EXIT_FAILURE);
+		} 
+		else if (pid > 0) {
+			exit(EXIT_SUCCESS);
+		}
+
+		umask(0);
+		sid = setsid();
+		if (sid < 0) {
+			exit(EXIT_FAILURE);
+		}						
+	}	
+	
+	g_option_context_free(context);
+	
+	/* get up and running */
+	g_set_prgname("gastify");
+	gtk_window_set_default_icon_name("gastify");
+  gtk_set_locale();
+	gtk_init(&argc, &argv);
+	
+	/* gettext */
+  setlocale (LC_ALL, "");
+  bindtextdomain("gastify", "/usr/share/locale");
+	textdomain("gastify");
+	
+	/* open socket and initialize statusicon */
+	watchSocket(port, initializeGui());
+	gtk_main ();
+	
+	return 0;
+}
