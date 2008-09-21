66b0f3f27d685f64cab3a26e5e0f735695270e01
penschuck <penschuck@8a573288-591e-0410-a4c7-0d60402f24be>
2008-09-21 11:31:04 +0000

    * license change in about window

--- src/gui.c
+++ src/gui.c
@@ -131,14 +131,15 @@ void activateMenu() {
 /* show about */
 void onShowAbout() {
 
-	gchar *license = g_strconcat("\nGastify is free software; you can redistribute it and/or modify it under", 
-					"the terms of the GNU General Public License as published by the Free Software Foundation",
-					"either version 2 of the License, or (at your option) any later version.\n\r",
-					"Gastify is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; ",
-					"without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. ",
-					"See the GNU General Public License for more details.\n\r You should have received a copy ",
-					"of the GNU General Public License along with Gastify; if not, write to the ",
-					"Free Software Foundation, Inc.,59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n",
+	gchar *license = g_strconcat("\ngastify is free software: you can redistribute it and/or modify ", 
+					"it under the terms of the GNU Lesser General Public License as published by ",
+					"the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\r",
+					"gastify is distributed in the hope that it will be useful, ",
+					"but WITHOUT ANY WARRANTY; without even the implied warranty of ",
+					"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the ",
+					"GNU General Public License for more details.\n\r",
+					"You should have received a copy of the GNU Lesser General Public License ",
+					"along with gastify.  If not, see <http://www.gnu.org/licenses/>.",
 					NULL);
 	
 	static const char *authors[] = {"Jan Penschuck","penschuck@gmail.com", NULL};
