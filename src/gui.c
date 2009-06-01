5d24e12ae767beecbd0fbe72c7de063ed3983b74
penschuck <penschuck@8a573288-591e-0410-a4c7-0d60402f24be>
2009-06-01 00:02:33 +0000

    * dropped glade dep in favor of GtkBuilder * fixed wrong init of GOptionEntry

--- src/gui.c
+++ src/gui.c
@@ -23,8 +23,6 @@
 #include <glib.h>
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
-#include <glade/glade.h>
-#include <glade/glade-xml.h>
 #include <libnotify/notify.h>
 #include <libnotify/notification.h>
 #include <gtk/gtkstatusicon.h>
@@ -36,39 +34,35 @@ static GtkWidget *menu;
 static GtkWidget *historyDialog;
 static GtkWidget *textView;
 static GtkTextBuffer *buffer;
-static GtkWidget *toggle;
+static GtkToggleAction *toggle;
 
 GtkStatusIcon* initializeGui() {
 
-	/* initialize statusicon */
 	GtkStatusIcon *icon;
-	GladeXML *xml;
-	gchar *gladefile;
+	GtkBuilder *builder;
+	gchar *uifile;
 	int x, y;
 	gboolean push_in;
-	
+
+	builder = gtk_builder_new();
+	uifile = g_strconcat(PACKAGE_DATA_DIR, "/gastify/gastify.ui", NULL);
+	gtk_builder_add_from_file (builder, uifile, NULL);
+	gtk_builder_connect_signals(builder, NULL);
+
+	/* intialize statusicon */	
 	icon = gtk_status_icon_new_from_icon_name("gastify");
 	gtk_status_icon_set_tooltip(icon, _("Gastify Call Notification"));
 
 	/* load menu */
-	gladefile = g_strconcat( PACKAGE_DATA_DIR, "/gastify/gastify.glade", NULL);
-	
-	xml = glade_xml_new(gladefile, "menu1", "gastify");
-	glade_xml_signal_autoconnect(xml);
-
-	menu = glade_xml_get_widget(xml, "menu1");
-	toggle = glade_xml_get_widget(xml, "notification-toggle");
-	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(toggle), TRUE);
+	menu = (GtkWidget*)gtk_builder_get_object(builder, "menu1");
+	toggle = (GtkToggleAction*)gtk_builder_get_object(builder, "notification-toggle");
 	gtk_status_icon_position_menu((GtkMenu*)menu, &x, &y, &push_in, icon);
 	g_signal_connect(icon, "popup-menu", GTK_SIGNAL_FUNC(activateMenu), NULL);
 	g_signal_connect(icon, "activate", GTK_SIGNAL_FUNC(onShowHistory), icon);
 
 	/* load history-window */
-	xml = glade_xml_new(gladefile, "window1", "gastify");
-	glade_xml_signal_autoconnect(xml);
-
-	historyDialog = glade_xml_get_widget(xml, "window1");
-	textView = glade_xml_get_widget(xml, "textview1");
+	historyDialog = (GtkWidget*)gtk_builder_get_object(builder, "window1");
+	textView = (GtkWidget*)gtk_builder_get_object(builder, "textview1");
 	buffer = gtk_text_buffer_new(NULL);
 	g_signal_connect(historyDialog, "delete-event", GTK_SIGNAL_FUNC(gtk_widget_hide_on_delete), NULL);
 
@@ -86,7 +80,7 @@ void notifyPopup(gchar *notifyMessage, GtkStatusIcon *icon) {
 	notify_notification_set_timeout(notify, popuptime*1000);
 	notify_notification_attach_to_status_icon(notify, icon);
 	
-	if ( gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(toggle))) {
+	if ( gtk_toggle_action_get_active((GtkToggleAction*)(toggle))) {
 		notify_notification_show(notify, NULL);
 	}
 	
@@ -131,24 +125,25 @@ void activateMenu() {
 /* show about */
 void onShowAbout() {
 
-	gchar *license = g_strconcat("\ngastify is free software: you can redistribute it and/or modify ", 
+	gchar *license = g_strconcat("\nGastify is free software: you can redistribute it and/or modify ", 
 					"it under the terms of the GNU Lesser General Public License as published by ",
 					"the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\r",
-					"gastify is distributed in the hope that it will be useful, ",
+					"Gastify is distributed in the hope that it will be useful, ",
 					"but WITHOUT ANY WARRANTY; without even the implied warranty of ",
 					"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the ",
 					"GNU General Public License for more details.\n\r",
 					"You should have received a copy of the GNU Lesser General Public License ",
 					"along with gastify.  If not, see <http://www.gnu.org/licenses/>.",
 					NULL);
+
+	gchar *authors[] = {"Jan Penschuck","penschuck@gmail.com", NULL};
 	
-	static const char *authors[] = {"Jan Penschuck","penschuck@gmail.com", NULL};
 	gtk_show_about_dialog(NULL,
 							"name", PACKAGE,
 							"version", VERSION,
 							"license", license,
 							"wrap-license", TRUE,
-							"copyright", "\xc2\xa9 2006-2008 Jan Penschuck",
+							"copyright", "\xc2\xa9 2006-2009 Jan Penschuck",
 							"comments", _("a client for app-notify"),
 							"logo-icon-name", "gastify",
 							"website", "http://code.google.com/p/gastify/",
