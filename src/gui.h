76900bb38590dc3efbbf6e406904dec4a744855c
azummo-google <azummo-google@towertech.it>
2009-09-22 21:49:08 +0000

    the cid list is now a tree view with copy to clipboard and google search options. added --call-cmd, --strip-leading the call command is triggered by a double click on the treeview or by the quick call text entry field.

--- src/gui.h
+++ src/gui.h
@@ -1,10 +1,2 @@
-/* This file was automatically generated.  Do not edit! */
-void onClearHistory();
-void onShowAbout();
-void addToHistory(gchar *call);
-extern gchar *execcmd;
-extern gint popuptime;
-void notifyPopup(gchar *notifyMessage,GtkStatusIcon *icon);
-void onShowHistory(gpointer *data);
-void activateMenu();
-GtkStatusIcon *initializeGui();
+extern void addToHistory(gchar *cid, gchar *extension, gchar *name);
+extern GtkStatusIcon * initializeGui(void);
