2e9bae5e6d82b7913043f68fa6962c3d88b563d7
penschuck <penschuck@8a573288-591e-0410-a4c7-0d60402f24be>
2009-10-05 16:29:27 +0000

    small changes and fixes in german translation and callscript.php

--- extras/callscript.php
+++ extras/callscript.php
@@ -1,6 +1,8 @@
 <?
+// this script uses the same license as gastify does
+// Monday, Oct 05 2009 Jan Penschuck
 
-// some vars
+// some vars to fill
 $host = "localhost";	 	// host where asterisk manager is running on
 $port = 5038;			// default is 5038
 $user = "admin";
@@ -11,21 +13,25 @@ $context = "from-internal";	// the context to use
 // here we go
 $number = rtrim(ltrim($_GET[number]));
 $CID = "Call <$number>";
-$socket = fsockopen ($host, $port);
-fputs($socket, "Action: login\r\n");
-fputs($socket, "Events: off\r\n");
-fputs($socket, "Username: $user\r\n");
-fputs($socket, "Secret: $pass\r\n\r\n");
-fputs($socket, "Action: originate\r\n");
-fputs($socket, "Channel: SIP/$ext\r\n");
-fputs($socket, "WaitTime: 30\r\n");
-fputs($socket, "CallerId: $CID\r\n");
-fputs($socket, "Exten: $number\r\n");
-fputs($socket, "Context: $context\r\n");
-fputs($socket, "Priority: 1\r\n\r\n");
-fputs($socket, "Action: Logoff\r\n\r\n");
-sleep(2);
-fclose($socket);
 
+if ($number != "") {
+	$socket = fsockopen ($host, $port);
+	fputs($socket, "Action: login\r\n");
+	fputs($socket, "Events: off\r\n");
+	fputs($socket, "Username: $user\r\n");
+	fputs($socket, "Secret: $pass\r\n\r\n");
+	fputs($socket, "Action: originate\r\n");
+	fputs($socket, "Channel: SIP/$ext\r\n");
+	fputs($socket, "WaitTime: 30\r\n");
+	fputs($socket, "CallerId: $CID\r\n");
+	fputs($socket, "Exten: $number\r\n");
+	fputs($socket, "Context: $context\r\n");
+	fputs($socket, "Priority: 1\r\n\r\n");
+	fputs($socket, "Action: Logoff\r\n\r\n");
+	sleep(2);
+	fclose($socket);
+} else {
+	echo "error: no telephonenumber given";
+}
 ?>
 
